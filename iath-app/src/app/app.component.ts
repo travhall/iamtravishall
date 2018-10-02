f8757426e52b7f0af5b3373955c91cabe703ade6
travhall <unkletas@gmail.com>
2018-10-02 12:47:46 -0600

    Added SCSS to assets and updated index

--- iath-app/src/app/app.component.ts
+++ iath-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'iath-app';
+  title =
+    "iamtravishall.com | the online portfolio of UX Designer and UI Developer Travis Hall";
 }
