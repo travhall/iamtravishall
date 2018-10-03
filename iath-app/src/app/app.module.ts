265bd7929b2525dcb4db93e4a8f4d6028c04f139
travhall <unkletas@gmail.com>
2018-10-03 11:51:33 -0600

    Added Layout components and removed unnecessary SCSS

--- iath-app/src/app/app.module.ts
+++ iath-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavComponent } from './layout/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
