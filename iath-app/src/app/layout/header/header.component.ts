32c7fae971cf0c232a60d2258fc06ac53fbca470
travhall <unkletas@gmail.com>
2019-01-03 09:09:13 -0700

    Quick updates to home and image optimizations

--- iath-app/src/app/layout/header/header.component.ts
+++ iath-app/src/app/layout/header/header.component.ts
@@ -4,24 +4,37 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-header',
   template: `
     <header>
-      <h1 class="hide">iamtravishall.com | the online portfolio of UX Designer and UI Developer Travis Hall</h1>
+      <h1 class="hide">
+        iamtravishall.com | portfolio of UX Designer and UI Developer Travis
+        Hall
+      </h1>
       <a routerLink="/" class="logo">
         <svg width="100" height="83">
           <use xlink:href="assets/symbol-defs.svg#icon-logo-iath"></use>
         </svg>
       </a>
-      <div class="links">
-        <a href="mailto:hello@iamtravishall.com" class="link-icon"target="_blank" title="Email me at hello@iamtravishall.com">
+      <!-- <div class="links">
+        <a
+          href="mailto:hello@iamtravishall.com"
+          class="link-icon"
+          target="_blank"
+          title="Email me at hello@iamtravishall.com"
+        >
           <svg class="icon">
             <use xlink:href="assets/symbol-defs.svg#icon-envelope"></use>
           </svg>
         </a>
-        <a href="https://www.linkedin.com/in/travhall/" class="link-icon" target="_blank" title="Find me on Linkedin">
+        <a
+          href="https://www.linkedin.com/in/travhall/"
+          class="link-icon"
+          target="_blank"
+          title="Find me on Linkedin"
+        >
           <svg class="icon">
             <use xlink:href="assets/symbol-defs.svg#icon-linkedin"></use>
           </svg>
         </a>
-      </div>
+      </div> -->
     </header>
   `,
   styles: []
--- iath-app/src/app/views/home/home.component.ts
+++ iath-app/src/app/views/home/home.component.ts
@@ -8,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
         <div class="hero__content">
           <h2>Hello, I'm Travis</h2>
           <h3 class="h4">&hellip; and I enjoy making things for the web.</h3>
-          <p>I currently oversee a team of very talented UX designers for <a href="https://www.arrow.com/" class="link" target="_blank" title="Arrow Electronics : Five Years Out">Arrow Electronics</a>. Previously, I wrote code for <a href="http://ideapark.com/" class="link" target="_blank" title="ideapark, doing good work since 1994">ideapark</a> and <a href="https://www.lacek.com/" class="link" target="_blank" title="The Lacek Group - A Specialist Brand of Company of the Ogilvy Group">The Lacek Group</a>. I’ve worked on projects for brands like <em>Target</em>, <em>General Mills</em>, <em>Enterprise &amp; National Car Rental</em> and <em>US Bank</em>. I’ve also worked with the fine people at <a href="http://www.playworkgroup.com/" class="link" target="_blank" title="play work group">playworkgroup</a>, <a href="https://www.adweek.com/agencyspy/denver-b2b-agency-faction-media-closes-after-12-years-in-business/135724" class="link" target="_blank" title="RIP Faction :("><del>Faction Media</del></a> and <a href="http://www.mccannmpls.com/" class="link" target="_blank" title="McCann Minneapolis">McCann</a>. Since you're here, see what I have to offer below or drop me a line at <a href="mailto:hello@iamtravishall.com" class="link"target="_blank" title="Email me at hello@iamtravishall.com">hello@iamtravishall.com</a>.</p>
-          <a [scrollTo]="'#expertise'" [scrollDuration]="500" [scrollOffset]="-90" class="btn -ghost" title="Skillz n' stuff">
-            See what I bring to the table<svg class="icon -sm">
-              <use xlink:href="assets/symbol-defs.svg#icon-chevron-outline-down"></use>
+          <p>
+            I'm a Denver based designer/developer, currently leading a team of
+            very talented UX designers for
+            <a
+              href="https://www.arrow.com/"
+              class="link"
+              target="_blank"
+              title="Arrow Electronics : Five Years Out"
+              >Arrow Electronics</a
+            >. Previously, I wrote code for
+            <a
+              href="http://ideapark.com/"
+              class="link"
+              target="_blank"
+              title="ideapark, doing good work since 1994"
+              >ideapark</a
+            >
+            and
+            <a
+              href="https://www.lacek.com/"
+              class="link"
+              target="_blank"
+              title="The Lacek Group - A Specialist Brand of Company of the Ogilvy Group"
+              >The Lacek Group</a
+            >, to name a few.
+          </p>
+          <a
+            [scrollTo]="'#expertise'"
+            [scrollDuration]="500"
+            [scrollOffset]="-90"
+            class="btn -ghost"
+            title="A very particular set of skills"
+          >
+            Expertise<svg class="icon -sm">
+              <use
+                xlink:href="assets/symbol-defs.svg#icon-chevron-outline-down"
+              ></use>
+            </svg>
+          </a>
+          <a
+            [scrollTo]="'#projects'"
+            [scrollDuration]="500"
+            [scrollOffset]="-90"
+            class="btn -ghost"
+            title="The proof is in the pudding"
+          >
+            Case Studies<svg class="icon -sm">
+              <use
+                xlink:href="assets/symbol-defs.svg#icon-chevron-outline-down"
+              ></use>
             </svg>
           </a>
         </div>
@@ -20,7 +66,11 @@ import { Component, OnInit } from '@angular/core';
       <section class="grid -primary" id="expertise">
         <div class="card -ux">
           <h4 class="display-1">User Experience Design</h4>
-          <p>I offer end-to-end user experience design. From user research to prototyping to user-centric design deliverables, I've got it covered.</p>
+          <p>
+            I offer end-to-end user experience design. From user research to
+            prototyping to user-centric design deliverables, I've got it
+            covered.
+          </p>
           <ul>
             <li>Dolor pulvinar etiam.</li>
             <li>Sagittis adipiscing.</li>
@@ -30,7 +80,11 @@ import { Component, OnInit } from '@angular/core';
         </div>
         <div class="card -ui">
           <h4 class="display-1">Front End &amp; UI Developement</h4>
-          <p>Pixel perfect may be a myth but I'm a unicorn. I can provide the next best thing, consistent, scalable, modular, responsive front end work.</p>
+          <p>
+            Pixel perfect may be a myth but I'm a unicorn. I can provide the
+            next best thing, consistent, scalable, modular, responsive front end
+            work.
+          </p>
           <ul>
             <li>Dolor pulvinar etiam.</li>
             <li>Sagittis adipiscing.</li>
@@ -40,7 +94,11 @@ import { Component, OnInit } from '@angular/core';
         </div>
         <div class="card -vis">
           <h4 class="display-1">Visual &amp; Graphic Design</h4>
-          <p>Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+          <p>
+            Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada
+            magna mollis euismod. Cras justo odio, dapibus ac facilisis in,
+            egestas eget quam.
+          </p>
           <ul>
             <li>Dolor pulvinar etiam.</li>
             <li>Sagittis adipiscing.</li>
@@ -51,13 +109,19 @@ import { Component, OnInit } from '@angular/core';
       </section>
 
       <section class="panel__container" id="projects">
-
         <div class="panel -arrow">
           <div class="panel__content -center">
             <h4 class="display-1">Arrow Electronics</h4>
+            <h4>Enterprise Ecommerce Platform</h4>
             <h4>UX Design Lead</h4>
             <h5>2016 - present</h5>
-            <button routerLink="/arrow" routerLinkActive="active" class="btn -ghost">Read More</button>
+            <button
+              routerLink="/arrow"
+              routerLinkActive="active"
+              class="btn -ghost"
+            >
+              Read More
+            </button>
           </div>
         </div>
 
@@ -67,7 +131,14 @@ import { Component, OnInit } from '@angular/core';
             <h4>Shopify Theme / Microsite</h4>
             <h4>Front End Development</h4>
             <h5>2014 - 2018</h5>
-            <button routerLink="/stuntpuppy" routerLinkActive="active" class="btn -ghost" disabled>Coming Soon</button>
+            <button
+              routerLink="/stuntpuppy"
+              routerLinkActive="active"
+              class="btn -ghost"
+              disabled
+            >
+              Coming Soon
+            </button>
           </div>
         </div>
 
@@ -77,7 +148,14 @@ import { Component, OnInit } from '@angular/core';
             <h4>Agency Microsite</h4>
             <h4>Front End Development</h4>
             <h5>2012 - 2014</h5>
-            <button routerLink="/lacek" routerLinkActive="active" class="btn -ghost" disabled>Coming Soon</button>
+            <button
+              routerLink="/lacek"
+              routerLinkActive="active"
+              class="btn -ghost"
+              disabled
+            >
+              Coming Soon
+            </button>
           </div>
         </div>
       </section>
